be0829b51ea58d88807e63292dc70cf03fe28c8b
carlos horacio lopez <krloss@gmail.com>
2022-10-13 22:02:00 -0300

    maven

--- src/main/java/ar/com/codoacodo/clase11/IGenerador.java
+++
@@ -1,8 +0,0 @@
-package ar.com.codoacodo.clase11;
-
-import java.util.Collection;
-
-public interface IGenerador {
-
-	public EstadisticaItem generar(Collection<Turno> turnos);
-}
--- src/main/java/ar/com/codoacodo/clase11/SearchService.java
+++
@@ -1,8 +0,0 @@
-package ar.com.codoacodo.clase11;
-
-import java.util.Collection;
-
-public interface SearchService {
-
-	public Collection<Turno> search();
-}
